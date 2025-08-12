f016ca36d909f194dc6904956164ce073d50df7a
dvijnru <nicolaa-designer@yandex.ru>
2025-08-12 23:20:14 +0500

    Переход на client

--- src/Verifier.php
+++ src/Verifier.php
@@ -2,7 +2,7 @@
 
 namespace WebsiteProtection\CaptchaHubVerifier;
 
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 
 class Verifier
@@ -29,19 +29,39 @@ class Verifier
             return false;
         }
 
+        $client = new Client(['timeout' =>5]);
         try {
-            $response = Http::asForm()
-                ->timeout(5)
-                ->post($this->verifyUrl, [
+            $response = $client->post($this->verifyUrl, [
+                'form_params' => [
                     'secret' => $secretKey,
-                    'token'  => $token,
-                ]);
+                    'token'  => $token
+                ]
+            ]);
 
-            return $response->successful() && $response->json('success') === true;
+            if ($response->getStatusCode() === 200) {
+                $result = json_decode($response->getBody()->getContents());
+                return $result && $result->success === true;
+            }
         } catch (\Exception $e) {
             Log::error('Captcha Hub service is unavailable.', ['error' => $e->getMessage()]);
             // В случае недоступности сервиса, лучше пропустить проверку
             return true;
         }
+
+//        try {
+//            $response = Http::asForm()
+//                ->timeout(5)
+//                ->post($this->verifyUrl, [
+//                    'secret' => $secretKey,
+//                    'token'  => $token,
+//                ]);
+//
+//            return $response->successful() && $response->json('success') === true;
+//        } catch (\Exception $e) {
+//            Log::error('Captcha Hub service is unavailable.', ['error' => $e->getMessage()]);
+//            // В случае недоступности сервиса, лучше пропустить проверку
+//            return true;
+//        }
+
     }
 }
